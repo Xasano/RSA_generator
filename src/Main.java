6301032f4bd99b95f78d11a4a30821c2262cc7d6
xasano <bastien.lefumeux@etu.unice.fr>
2023-02-09 14:17:05 -0500

    Ajout numérisation et découpage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 4;
+        BlocNum test = new BlocNum("SaLut",n);
+        int[] resASCII = test.getStrNum();
+        String[] res = test.getStrTrans();
+        for (String re : res) {
+            System.out.println(re);
+        }
+        for (int j : resASCII) {
+            System.out.print(j);
+        }
     }
 }
