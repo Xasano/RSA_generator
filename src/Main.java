c8fb59cd60953f6e141be7ef11f6e41335f01c03
baldojerome <99471537+jeromebaldo@users.noreply.github.com>
2023-02-09 19:06:17 -0500

    DERNI7RE  PUSH

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,22 @@ public class Main {
         ///////creation du module RSA pour alice/////////////
         System.out.println("CREATION DU RSA SELON LES CLES D'ALICE");
         RSA rsaAlice = new RSA(clesAlice);
-
+        System.out.println("");
         /////////récupération et traitement du texte/////////
+        System.out.println("RECUPERATION ET TRAITEMENT DU TEXTE");
         System.out.println("bob tape son texte pour Alice");
         System.out.println("numerisation + decoupage du texte de bob");
         //c'est là que tu mets ta production bastien
+        System.out.println("");
 
         /////OPERATION DE CRYPTAGE ////////////
+        System.out.println("CRYPTAGE DU TEXTE AVEC CLE PUBLIC D'ALICE");
         //appel de la fonction avec RSA d'alice, RSA crypte avec clé public d'alice
         //affichage du texte crypté
+        System.out.println("");
 
         /////////////OPERATION DE DECRYPTAGE////////////
+        System.out.println("DECRYPTAGE DU TEXTE  CHIFFRE AVEC CLE PRIVEEE D'ALICE");
         //alice va  utilisé son RSA + clé privé pour  décrypter
         //affichage du message en clair
     }
